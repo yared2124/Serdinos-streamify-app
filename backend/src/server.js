4b7cf37a81055a35cd57dae4fe54407788319350
yared aregayehu <yaredbaby21@gmail.com>
2026-06-04 17:26:30 -0500

    on server.js file inserted

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,14 @@
 import express from 'express';
 import  "dotenv/config"
 
-
-
+import authRoutes from './routes/auth.route.js'
 const app = express();
 const PORT =process.env.PORT
 
-app.get('/api/auth/signup',(req,res)=>{
-    res.send("Signup Route");
-})
 
-
-app.get("/api/auth/login", (req, res) => {
-  res.send("login Route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  res.send("logout Route");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`); 
 })
-
 
